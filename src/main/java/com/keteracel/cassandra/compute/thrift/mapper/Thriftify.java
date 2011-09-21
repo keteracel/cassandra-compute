314e905726e9b47f53c548001f83c5707b61f8aa
Paul_Loy <Paul_Loy@scee.net>
2011-09-21 23:23:02 +0100

    thriftification progresses

--- src/main/java/com/keteracel/cassandra/compute/thrift/mapper/Thriftify.java
+++ src/main/java/com/keteracel/cassandra/compute/thrift/mapper/Thriftify.java
@@ -1,12 +1,34 @@
 package com.keteracel.cassandra.compute.thrift.mapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.keteracel.cassandra.compute.thrift.annotations.ThriftInterface;
+
 public class Thriftify {
 
 	public Thriftify() {
 	}
 
-	public <T> T instantiate(Class<T> type) {
-
+	private final Map<Class<?>,Object> instances = new HashMap<Class<?>, Object>();
+	public <T> T maybeProxy(Class<T> type) {
+		Object o = instances.get(type);
+		if (o != null) {
+			return (T) o;
+		}
+		
+		if (type.getAnnotation(ThriftInterface.class) == null) {
+			return instantiate(type);
+		} else {
+			return proxy(type);
+		}
+	}
+	
+	private <T> T instantiate(Class<T> type) {
+		return null;
+	}
+	
+	private <T> T proxy(Class<T> type) {
 		return null;
 	}
 
