f42092104f01e8f67f38265690a5aec1175962b3
Paul_Loy <Paul_Loy@scee.net>
2011-09-21 23:11:12 +0100

    we want to be able to thriftify services ans objects so we can send them over thrift

---
+++ src/main/java/com/keteracel/cassandra/compute/thrift/annotations/ThriftMethod.java
@@ -0,0 +1,15 @@
+package com.keteracel.cassandra.compute.thrift.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Paul_Loy
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ThriftMethod {
+
+}
---
+++ src/main/java/com/keteracel/cassandra/compute/thrift/mapper/Thriftify.java
@@ -0,0 +1,13 @@
+package com.keteracel.cassandra.compute.thrift.mapper;
+
+public class Thriftify {
+
+	public Thriftify() {
+	}
+
+	public <T> T instantiate(Class<T> type) {
+
+		return null;
+	}
+
+}
