7be5288f88ed75aa1585dce76a41785d4b12ee52
Paul_Loy <Paul_Loy@scee.net>
2011-09-21 23:58:00 +0100

    more thrift progress, thinking about Spring...

--- src/main/java/com/keteracel/cassandra/compute/thrift/mapper/Thriftify.java
+++ src/main/java/com/keteracel/cassandra/compute/thrift/mapper/Thriftify.java
@@ -5,18 +5,15 @@ import java.util.Map;
 
 import com.keteracel.cassandra.compute.thrift.annotations.ThriftInterface;
 
+/**
+ * @author Paul_Loy
+ */
 public class Thriftify {
 
 	public Thriftify() {
 	}
 
-	private final Map<Class<?>,Object> instances = new HashMap<Class<?>, Object>();
 	public <T> T maybeProxy(Class<T> type) {
-		Object o = instances.get(type);
-		if (o != null) {
-			return (T) o;
-		}
-		
 		if (type.getAnnotation(ThriftInterface.class) == null) {
 			return instantiate(type);
 		} else {
@@ -24,11 +21,50 @@ public class Thriftify {
 		}
 	}
 	
-	private <T> T instantiate(Class<T> type) {
-		return null;
+	private final Map<Class<?>,Object> instances = new HashMap<Class<?>, Object>();
+	protected <T> T instantiate(Class<T> type) {
+		Object o = instances.get(type);
+		if (o != null) {
+			return (T) o;
+		}
+		
+		synchronized (instances) {
+			o = instances.get(type);
+			if (o != null) {
+				return (T) o;
+			}
+			
+			try {
+				T object = type.getConstructor().newInstance();
+				instances.put(type, object);
+				return object;
+			} catch (Exception e) {
+				throw new RuntimeException("possibly no zero-args constructor on your service?", e);
+			}
+		}
 	}
 	
-	private <T> T proxy(Class<T> type) {
+	private final Map<Class<?>,Object> proxies = new HashMap<Class<?>, Object>();
+	private synchronized <T> T proxy(Class<T> type) {
+		Object o = proxies.get(type);
+		if (o != null) {
+			return (T) o;
+		}
+		
+		synchronized (proxies) {
+			o = proxies.get(type);
+			if (o != null) {
+				return (T) o;
+			}
+			
+			T t = wrap(instantiate(type));
+			proxies.put(type, t);
+			return t;
+		}
+	}
+	
+	public synchronized <T> T wrap(T object) {
+		//TODO:
 		return null;
 	}
 
